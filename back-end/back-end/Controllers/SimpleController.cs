5298d64d2c0ad9884a594c858864ad73308f9708
André Rodrigues da Silva <andre.002016@gmail.com>
2021-02-14 17:46:45 -0300

    Projeto back end

---
+++ back-end/back-end/Controllers/SimpleController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace back_end.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SimpleController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("currencies")]
+        public ActionResult<string> Get(int id)
+        {
+            return Json( new { response = "200 OK" } );
+        }
+
+
+    }
+}
